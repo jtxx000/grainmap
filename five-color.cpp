d01fa602887dcfa9c08ed85260ffd176dfabf354
Caleb Reach <jtxx000@gmail.com>
2011-06-18 21:07:53 -0400

    reformatted five-color.cpp

--- five-color.cpp
+++ five-color.cpp
@@ -6,41 +6,41 @@ using namespace std;
 
 #include <stdio.h>
 
-enum VertexType {sNone, s4T, s5T};
+enum vertex_type {s_none, s_4t, s_5t};
 
-struct Vertex;
-struct Edge;
+struct vertex;
+struct edge;
 
-typedef list<Vertex*> VertexStack;
+typedef list<vertex*> vertex_stack;
 
-struct EdgeIterator {
+struct edge_iterator {
   int i;
-  Edge* edge;
+  edge* e;
 
-  EdgeIterator(Vertex* v);
+  edge_iterator(vertex* v);
 
-  Edge* operator->() {
-    return edge;
+  edge* operator->() {
+    return e;
   }
 };
 
-#define foreachEdge(edge, vertex) for (EdgeIterator edge(vertex); --edge.i >=0; edge.edge = edge.edge->next)
+#define foreach_edge(edge, vertex) for (edge_iterator edg(vertex); --edg.i >=0; edg.e = edg.e->next)
 
-typedef char VertexColour;
+typedef char vertex_color;
 
-struct Edge {
-  Vertex* vertex;
-  Edge* pos;
-  Edge* prev;
-  Edge* next;
+struct edge {
+  vertex* vtx;
+  edge* pos;
+  edge* prev;
+  edge* next;
 
   void remove() {
     prev->next = next;
     next->prev = prev;
   }
 
-  void identify(Edge* other) {
-    if (other->vertex == vertex) {
+  void identify(edge* other) {
+    if (other->vtx == vtx) {
       other->remove();
       other = other->prev;
     }
@@ -52,113 +52,113 @@ struct Edge {
   }
 };
 
-struct Vertex {
-  Edge* rootEdge;
+struct vertex {
+  edge* root_edge;
   int degree;
-  VertexType type;
-  VertexStack::iterator stackPos;
-  VertexColour colour;
+  vertex_type type;
+  vertex_stack::iterator stack_pos;
+  vertex_color color;
   bool mark;
 
-  Vertex() : rootEdge(0), degree(0), type(sNone), colour(0), mark(false) {
+  vertex() : root_edge(0), degree(0), type(s_none), color(0), mark(false) {
   }
 
   void remove() {
     printf("remove %p %d\n", this, degree);
-    foreachEdge(edge, this) {
-      printf("remove from edge %p %d\n", edge->vertex, edge->vertex->degree);
-      if (edge->pos == edge->vertex->rootEdge)
-        edge->vertex->rootEdge = edge->pos->next;
-      edge->pos->remove();
-      --edge->vertex->degree;
+    foreach_edge(edg, this) {
+      printf("remove from edge %p %d\n", edg->vtx, edg->vtx->degree);
+      if (edg->pos == edg->vtx->root_edge)
+        edg->vtx->root_edge = edg->pos->next;
+      edg->pos->remove();
+      --edg->vtx->degree;
     }
   }
 
-  bool adjacentTo(Vertex* other) {
-    foreachEdge(edge, this) {
-      if (edge->vertex == other)
+  bool adjacent_to(vertex* other) {
+    foreach_edge(edg, this) {
+      if (edg->vtx == other)
         return true;
     }
 
     return false;
   }
 
-  void assignColour() {
-    foreachEdge(edge, this) {
-      printf("neighbor-col: %p %d\n", edge->vertex, edge->vertex->colour);
-      colour |= 1<<edge->vertex->colour;
+  void assign_color() {
+    foreach_edge(edg, this) {
+      printf("neighbor-col: %p %d\n", edg->vtx, edg->vtx->color);
+      color |= 1<<edg->vtx->color;
     }
 
-    colour = ~colour;
+    color = ~color;
 
     for (int i=0; i<5; i++) {
-      if (colour & 1<<i) {
-        colour = i;
-        printf("colour found: %d\n", colour);
+      if (color & 1<<i) {
+        color = i;
+        printf("color found: %d\n", color);
         return;
       }
     }
 
-    printf("default colour\n");
+    printf("default color\n");
 
-    colour = 0;
+    color = 0;
   }
 
-  Edge* findMinEdge() {
-    Edge* edge = rootEdge;
-    Edge* minEdge = edge;
-    int minDeg = minEdge->vertex->degree;
+  edge* find_min_edge() {
+    edge* edg = root_edge;
+    edge* min_edge = edg;
+    int min_deg = min_edge->vtx->degree;
 
     for (int i=degree; --i>0;) {
-      if ((edge = edge->next)->vertex->degree < minDeg) {
-        minEdge = edge;
-        minDeg = edge->vertex->degree;
+      if ((edg = edg->next)->vtx->degree < min_deg) {
+        min_edge = edg;
+        min_deg = edg->vtx->degree;
       }
     }
 
-    return minEdge;
+    return min_edge;
   }
 
-  void removeFromStack(VertexStack* s4, VertexStack* s5) {
+  void remove_from_stack(vertex_stack* s4, vertex_stack* s5) {
     switch (type) {
-    case s4T:
-      s4->erase(stackPos);
+    case s_4t:
+      s4->erase(stack_pos);
       break;
-    case s5T:
-      s5->erase(stackPos);
+    case s_5t:
+      s5->erase(stack_pos);
     }
 
-    type = sNone;
+    type = s_none;
   }
 
-  void adjustS5(VertexStack* s4, VertexStack* s5) {
-    if (type != s5T)
+  void adjust_s5(vertex_stack* s4, vertex_stack* s5) {
+    if (type != s_5t)
       return;
 
-    foreachEdge(edge, this) {
-      if (edge->vertex->degree <= 6)
+    foreach_edge(edg, this) {
+      if (edg->vtx->degree <= 6)
         return;
     }
 
-    removeFromStack(s4,s5);
+    remove_from_stack(s4,s5);
   }
 
-  void popFrom(VertexStack* s4, VertexStack* s5) {
-    removeFromStack(s4,s5);
+  void pop_from(vertex_stack* s4, vertex_stack* s5) {
+    remove_from_stack(s4,s5);
 
     if (degree <= 6) {
-      foreachEdge(edge, this) {
-        edge->vertex->adjustS5(s4,s5);
+      foreach_edge(edge, this) {
+        edg->vtx->adjust_s5(s4,s5);
       }
     }
   }
 
   bool distinct() {
-    Edge* edge = rootEdge;
+    edge* edge = root_edge;
     int i=degree;
     bool r = true;
     for (;--i>=0; edge = edge->next) {
-      Vertex* v = edge->vertex;
+      vertex* v = edge->vtx;
       if (v->mark) {
         edge = edge->prev;
         r = false;
@@ -168,63 +168,63 @@ struct Vertex {
     }
 
     for (;++i<degree; edge = edge->prev)
-      edge->vertex->mark = false;
+      edge->vtx->mark = false;
 
     return r;
   }
 
-  void pushTo(VertexStack* s4, VertexStack* s5) {
+  void push_to(vertex_stack* s4, vertex_stack* s5) {
     if (degree > 5)
       return;
 
-    VertexStack* curStack = 0;
+    vertex_stack* cur_stack = 0;
 
     if (degree == 5 && distinct()) {
-      foreachEdge(edge, this) {
-        if (edge->vertex->degree <= 6) {
-          curStack = s5;
-          type = s5T;
+      foreach_edge(edg, this) {
+        if (edg->vtx->degree <= 6) {
+          cur_stack = s5;
+          type = s_5t;
           break;
         }
       }
-      if (!curStack) return;
+      if (!cur_stack) return;
     } else {
-      if (type == s4T)
+      if (type == s_4t)
         return;
 
-      removeFromStack(s4,s5);
-      curStack = s4;
-      type = s4T;
+      remove_from_stack(s4,s5);
+      cur_stack = s4;
+      type = s_4t;
     }
 
-    curStack->push_front(this);
-    stackPos = curStack->begin();
+    cur_stack->push_front(this);
+    stack_pos = cur_stack->begin();
   }
 };
 
-EdgeIterator::EdgeIterator(Vertex* v) : i(v->degree), edge(v->rootEdge) {
+edge_iterator::edge_iterator(vertex* v) : i(v->degree), e(v->root_edge) {
 }
 
-void fiveColour(vector<Vertex*>& xs) {
-  VertexStack s4, s5;
-  stack<pair<Vertex*, Vertex*> > sd;
+void five_color_vertices(vector<vertex*>& xs) {
+  vertex_stack s4, s5;
+  stack<pair<vertex*, vertex*> > sd;
 
   printf("size: %d\n", xs.size());
 
-  for (vector<Vertex*>::iterator v=xs.begin(); v != xs.end(); ++v)
-    (**v).pushTo(&s4,&s5);
+  for (vector<vertex*>::iterator v=xs.begin(); v != xs.end(); ++v)
+    (**v).push_to(&s4,&s5);
 
   for (;;) {
     while (!s4.empty()) {
-      Vertex* v = s4.front();
+      vertex* v = s4.front();
       printf("\n== pop s4 %p %d ==\n", v, v->degree);
       s4.pop_front();
       v->remove();
-      sd.push(pair<Vertex*,Vertex*>(v,0));
-      // Degree has decreased by one
-      foreachEdge(edge, v) {
-        printf("s4 edge %p\n", edge->vertex);
-        edge->vertex->pushTo(&s4, &s5);
+      sd.push(pair<vertex*,vertex*>(v,0));
+      // degree has decreased by one
+      foreach_edge(edg, v) {
+        printf("s4 edge %p\n", edg->vtx);
+        edg->vtx->push_to(&s4, &s5);
       }
     }
 
@@ -233,94 +233,94 @@ void fiveColour(vector<Vertex*>& xs) {
 
     printf("pop s5\n");
 
-    Vertex* v = s5.front();
+    vertex* v = s5.front();
     s5.pop_front();
     v->remove();
-    sd.push(pair<Vertex*,Vertex*>(v,0));
-    Edge *v1=v->findMinEdge(), *v2=v1->next, *v3=v2->next, *v4=v3->next, *a, *b;
-    if (v1->vertex->adjacentTo(v3->vertex)) {
+    sd.push(pair<vertex*,vertex*>(v,0));
+    edge *v1=v->find_min_edge(), *v2=v1->next, *v3=v2->next, *v4=v3->next, *a, *b;
+    if (v1->vtx->adjacent_to(v3->vtx)) {
       a = v1; b = v3;
     } else {
       a = v2; b = v4;
     }
 
-    a->vertex->popFrom(&s4,&s5);
-    b->vertex->popFrom(&s4,&s5);
+    a->vtx->pop_from(&s4,&s5);
+    b->vtx->pop_from(&s4,&s5);
     b->identify(a);
-    foreachEdge(edge, v) {
-      edge->vertex->pushTo(&s4,&s5);
+    foreach_edge(edg, v) {
+      edg->vtx->push_to(&s4,&s5);
     }
-    sd.push(pair<Vertex*,Vertex*>(a->vertex,b->vertex));
+    sd.push(pair<vertex*,vertex*>(a->vtx,b->vtx));
   }
 
   while (!sd.empty()) {
-    pair<Vertex*,Vertex*> v(sd.top());
-    printf("colour %p\n", v.first);
+    pair<vertex*,vertex*> v(sd.top());
+    printf("color %p\n", v.first);
     sd.pop();
     if (v.second)
-      v.first->colour = v.second->colour;
+      v.first->color = v.second->color;
     else
-      v.first->assignColour();
+      v.first->assign_color();
   }
 }
 
 extern "C" {
-  struct FiveColour {
-    struct FCVertex {
-      Edge* edges;
+  struct five_color {
+    struct fc_vertex {
+      edge* edges;
       int numEdges;
-      Vertex vertex;
+      vertex vtx;
     }* vertices;
     int numVertices;
   };
 
-  FiveColour* newFiveColour(int numVertices) {
-    FiveColour* fc = new FiveColour();
-    fc->vertices = new FiveColour::FCVertex[numVertices];
+  five_color* newFiveColor(int numVertices) {
+    five_color* fc = new five_color();
+    fc->vertices = new five_color::fc_vertex[numVertices];
     fc->numVertices = numVertices;
     return fc;
   }
 
-  void deleteFiveColour(FiveColour* c) {
+  void deletefive_color(five_color* c) {
     for (int i=0; i<c->numVertices; i++)
       delete c->vertices[i].edges;
     delete c->vertices;
     delete c;
   }
 
-  FiveColour::FCVertex* initVertex(FiveColour* c, int vertex, int size) {
-    FiveColour::FCVertex* v = c->vertices + vertex;
-    printf("init %p %d\n", &v->vertex, size);
-    v->edges = new Edge[size];
+  five_color::fc_vertex* initVertex(five_color* c, int vertex, int size) {
+    five_color::fc_vertex* v = c->vertices + vertex;
+    printf("init %p %d\n", &v->vtx, size);
+    v->edges = new edge[size];
     v->numEdges = size;
-    v->vertex.degree = size;
+    v->vtx.degree = size;
     for (int i=0; i<v->numEdges; i++) {
       v->edges[i].next = v->edges + (i+1)%v->numEdges;
       v->edges[i].prev = v->edges + (i-1+v->numEdges)%v->numEdges;
     }
     if (size)
-      v->vertex.rootEdge = v->edges;
+      v->vtx.root_edge = v->edges;
     return v;
   }
 
-  void setEdge(FiveColour* c, FiveColour::FCVertex* fromVertex, int fromEdge, int toVertex, int toEdge) {
-    Edge* e = fromVertex->edges + fromEdge;
-    FiveColour::FCVertex* v = c->vertices + toVertex;
-    e->vertex = &v->vertex;
-    printf("set edge %p:%d -> %d/%p:%d\n", &fromVertex->vertex, fromEdge, toVertex, e->vertex, toEdge);
+  void setEdge(five_color* c, five_color::fc_vertex* fromVertex, int fromEdge, int toVertex, int toEdge) {
+    edge* e = fromVertex->edges + fromEdge;
+    five_color::fc_vertex* v = c->vertices + toVertex;
+    e->vtx = &v->vtx;
+    printf("set edge %p:%d -> %d/%p:%d\n", &fromVertex->vtx, fromEdge, toVertex, e->vtx, toEdge);
     e->pos = v->edges + toEdge;
   }
 
-  int vertexColour(FiveColour::FCVertex* v) {
-    return v->vertex.colour;
+  int vertexColor(five_color::fc_vertex* v) {
+    return v->vtx.color;
   }
 
-  void runFiveColour(FiveColour* c) {
-    vector<Vertex*> vs;
+  void run_five_color(five_color* c) {
+    vector<vertex*> vs;
     vs.reserve(c->numVertices);
     for (int i=0; i<c->numVertices; i++)
-      vs.push_back(&c->vertices[i].vertex);
-    fiveColour(vs);
+      vs.push_back(&c->vertices[i].vtx);
+    five_color_vertices(vs);
   }
 }
 
@@ -330,29 +330,29 @@ extern "C" {
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(init) {
-  Vertex v;
+  vertex v;
   BOOST_CHECK(v.degree == 0);
 }
 
-void checkMarks(Vertex* v) {
-  foreachEdge(edge, v) {
-    BOOST_CHECK(!edge->vertex->mark);
+void checkMarks(vertex* v) {
+  foreach_edge(edg, v) {
+    BOOST_CHECK(!edg->vtx->mark);
   }
 }
 
 BOOST_AUTO_TEST_CASE(distinct) {
-  Vertex v;
-  Vertex v1,v2,v3;
-  Edge e1,e2,e3;
+  vertex v;
+  vertex v1,v2,v3;
+  edge e1,e2,e3;
   v.degree = 3;
-  e1.vertex = &v1; e1.prev = &e3; e1.next = &e2;
-  e2.vertex = &v2; e2.prev = &e1; e2.next = &e3;
-  e3.vertex = &v3; e3.prev = &e2; e3.next = &e1;
-  v.rootEdge = &e1;
+  e1.vtx = &v1; e1.prev = &e3; e1.next = &e2;
+  e2.vtx = &v2; e2.prev = &e1; e2.next = &e3;
+  e3.vtx = &v3; e3.prev = &e2; e3.next = &e1;
+  v.root_edge = &e1;
 
   BOOST_CHECK(v.distinct());
   checkMarks(&v);
-  e2.vertex = &v1;
+  e2.vtx = &v1;
   BOOST_CHECK(!v.distinct());
   checkMarks(&v);
 }
